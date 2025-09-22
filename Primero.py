52e2782aafb6abd963f966fc5ba5cabd2817552e
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-09-22 21:31:26 +0200

    Vamonos ya a casitaaaa

--- Primero.py
+++ Primero.py
@@ -5,3 +5,52 @@
 
 precioNeto = 100
 print(precioNeto)
+
+# • Crea una constante y muestra su contenido en pantalla.
+# • Cambia el valor de la constante y muéstralo en pantalla. ¿Has obtenido algún
+# error al cambiar el valor de la constante?
+# No
+
+CONSTANTE = 1
+print(CONSTANTE)
+
+CONSTANTE = 2
+print(CONSTANTE)
+
+# 1. Pedir por teclado el nombre y la edad de una persona y mostrar en pantalla un mensaje
+# similar a: “Te llamas _____ y tienes ___ años.”
+# 2. Ampliar el ejercicio anterior pidiendo la altura de la persona.
+# 3. Pedir por teclado el radio de un cÌrculo y mostrar su longitud y su area.
+
+# PI=3.1416
+
+# nombre = input("Introduce tu nombre: ")
+# edad = input("Introduce tu edad: ")
+
+# print("Te llamas ", nombre, " y tienes ", edad, " anios.")
+
+# altura = input("Introduce tu altura: ")
+# print("Te llamas ", nombre, " tienes ", edad, " anios y tu altura es ", altura)
+
+# radio = float(input("Introduce el radio del circulo: "))
+# longitud=PI**float(2)**radio
+# print("Radio: ", radio)
+
+# area=PI**(radio**radio)
+# print("Area: ", area)
+
+# Realiza las operaciones anteriores y muestra su contenido.
+# • Realiza el calculo 23 entre 10 y muestra el cociente y el resto. Verifica el resultado.
+# • Pide dos numeros por teclado y muestra el cociente y resto. Muestra tambiÈn la divisiÛn
+# entera.
+# print("Cociente: ", 23//10)
+# print("Resto: ", 23%10)
+
+# numero1=int(input("Introduce un numero: "))
+# numero2=int(input("Introduce otro numero: "))
+
+# print("Cociente: ",numero1//numero2)
+# print("Resto: ", numero1%numero2)
+
+print("Redondeo: ",round(3.9))
+print("Truncado: ",int(3.9))
