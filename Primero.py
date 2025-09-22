e3dd1ee13449f550320a05bf1720d7f37582aed1
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-09-22 20:37:42 +0200

    Primer ejercicio

---
+++ Primero.py
@@ -0,0 +1,7 @@
+# • Crea una variable llamada precioNeto y asígnale el valor 100
+# • Ejecuta la sentencia print(precioneto).
+# • ¿Qué ha ocurrido?
+# Ha mostrado el valor de la variable precioNeto
+
+precioNeto = 100
+print(precioNeto)
