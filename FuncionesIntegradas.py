23d3994ec03365323dae0afd82f085e52ad15dda
MiguelAngel313 <rmiguelangel222@gmail.com>
2025-10-01 19:42:08 +0200

    Ejercicios funciones integradas terminados.

--- FuncionesIntegradas.py
+++ FuncionesIntegradas.py
@@ -18,9 +18,21 @@ print("Dos decimales: ",round(media,2))
 # cambiar.
 print("Lista ordenada: ",sorted(numeros))
 # 9. Ordena los elementos de la lista y muéstralos.
+numeros = sorted(numeros)
+print("Numeros ordenados: ",numeros)
 # 10. ¿Qué diferencia hay entre los dos ejercicios anteriores?
+#El ejercicio 9 los ordena y los muestra pero sin llegar a hacer permanente el cambio de posicion mientras que en el ejercicio 10 primero se modifica el orden dejandolo guardado y despues se muestra
 # 11. Crea una lista de 5 números con range y muéstrala.
+lista1=list(range(5))
+print("Lista con range: ",lista1)
 # 12. Crea una lista de los 10 primeros números pares usando range y muéstrala.
+listaPares=list(range(0, 10, 2))
+print("Lista pares: ",listaPares)
 # 13. Crea una lista con los siguientes animales: zorro, elefante, gallina
+animales = ["zorro", "elefante", "gallina"]
+print("Lista animales: ", animales)
 # 14. Ordena alfabéticamente la lista.
+animales.sort()
+print("Lista de animales ordenada: ", animales)
 # 15. Redondea el número 7.856 a dos decimales.
+print("7.856 redondeado a dos decimales: ", round(7.856, 2))
