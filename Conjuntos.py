b0df97ec50d65e819dd5a529ca2839fa96572814
Miguel Angel <rmiguelangel222@gmail.com>
2025-11-04 18:40:32 +0100

    Continuar por nombres de amigos que se encuen

---
+++ Conjuntos.py
@@ -0,0 +1,57 @@
+# Crea un conjunto con los números del 1 al 5 e imprime todos sus elementos.
+
+conjunto = {1, 2, 3, 4, 5}
+print("Conjunto: ",conjunto)
+
+# Escribe un programa que elimine los elementos duplicados de una lista usando conjuntos.
+
+lista = [1, 1, 4, 5, 5, 7]
+conjunto = set(lista)
+print("Lista sin duplicados: ",conjunto)
+
+# Crea dos conjuntos con números del 1 al 10 y del 5 al 15, e imprime su intersección.
+
+menor = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+mayor = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+print("Interseccion",menor.intersection(mayor))
+
+# Crea dos conjuntos con números del 1 al 10 y del 5 al 15, e imprime su unión.
+
+print("Union: ", menor.union(mayor))
+
+# Crea dos conjuntos con números del 1 al 10 y del 5 al 15, e imprime su diferencia.
+
+print("Diferencia: ", menor.difference(mayor))
+
+# Crea un conjunto con los nombres de tus amigos y verifica si un nombre específico está en el conjunto.
+
+nombres = {'Ivan', 'Emily', 'Saray', 'Mario', 'Carlos', 'Pablo'}
+
+
+# Crea un conjunto vacío, agrega cinco elementos con el método add(), e imprime el resultado.
+
+# Escribe un programa que elimine un elemento dado de un conjunto (si existe).
+
+# Convierte una cadena en un conjunto de letras únicas (sin repetir) y muéstralo.
+
+# Crea un conjunto con tres elementos y elimina todos los elementos usando clear()
+
+# Dado un conjunto de frutas, pide al usuario una fruta y añade al conjunto si no existe.
+
+# Crea dos conjuntos y muestra los elementos que están en el primero pero no en el segundo.
+
+# Escribe un programa que determine si dos conjuntos son disjuntos (no comparten elementos).
+
+# Usa el método update() para combinar dos conjuntos.
+
+# Crea un conjunto con números aleatorios del 1 al 20 (sin repetidos) y muestra cuántos elementos contiene.
+
+# Dado un conjunto de palabras, muestra cuántas letras únicas hay en todas ellas combinadas.
+
+# Escribe un programa que verifique si un conjunto A es subconjunto o superconjunto de otro conjunto B.
+
+# Dado un conjunto de números, elimina todos los números pares.
+
+# Crea dos conjuntos con nombres y muestra los nombres que están en ambos.
+
+# Dado un conjunto de colores, conviértelo en una lista, ordénala alfabéticamente e imprímela.
